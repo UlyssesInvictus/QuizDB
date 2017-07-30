f71c3daffb502d8af7408b8fc2403dc9d4dd5cc8
Raynor Kuang <raynorkuang@gmail.com>
2017-07-30 16:04:52 -0400

    Add ESLint

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    window.fetch('api/questions')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error))
+  }
+
+
   render() {
     return (
       <div className="App">
