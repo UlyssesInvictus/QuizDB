04f5946fc1aa86612d9e10c57713a45f1b4f28d0
Raynor Kuang <raynorkuang@gmail.com>
2017-08-09 00:17:43 -0400

    Add error types

--- app/models/error.rb
+++ app/models/error.rb
@@ -1,3 +1,20 @@
 class Error < ApplicationRecord
   belongs_to :errorable, polymorphic: true
+
+  enum error_type: {
+    other: 0,
+    question: 1,
+    question_info: 2,
+    question_formatting: 3,
+  }
+
+  def error_type_description
+    {
+      other: "Any issue not included above.",
+      question: "Incomplete or missing question text.",
+      question_info: "Incomplete or inaccurate question info (tournament, category, etc.).",
+      question_formatting: "Formatting or display issues with question text or info."
+    }[error_type]
+  end
+
 end
