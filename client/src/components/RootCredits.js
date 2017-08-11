0e993455b23d67bab843f40c27861c94e0a99634
Raynor Kuang <raynorkuang@gmail.com>
2017-08-11 01:32:54 -0400

    Update root credits with forum profile link

--- client/src/components/RootCredits.js
+++ client/src/components/RootCredits.js
@@ -9,10 +9,11 @@ import {
 
 class Root extends React.Component {
   render() {
+    let raynorForumLink = "http://hsquizbowl.org/forums/memberlist.php?mode=viewprofile&u=5867";
     return  <div className="root-credits"><Container>
       <Header size='medium' textAlign='center' color='grey'>
         <p>
-          {"Created by Raynor Kuang <>"}
+          Created by <a href={raynorForumLink} target="_blank" rel="noopener noreferrer">Raynor Kuang</a> {"<>"}
           {/* <Icon fitted name='question' style={{marginRight: 0}}/> */}
           {" Inspired by "}
           <a href="http://quinterest.org/" target="_blank" rel="noopener noreferrer">Quinterest</a>
