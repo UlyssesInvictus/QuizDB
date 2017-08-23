1854fbb92db09492169ddbcab115779733f343df
Raynor Kuang <raynorkuang@gmail.com>
2017-08-22 21:16:11 -0400

    Add roles to AdminUser page

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,10 +1,14 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation, :role
 
+  config.sort_order = 'email_asc'
+  config.per_page = [10, 30, 50, 100]
+
   index do
     selectable_column
     id_column
     column :email
+    column :role
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -12,6 +16,7 @@ ActiveAdmin.register AdminUser do
   end
 
   filter :email
+  filter :role
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
@@ -19,6 +24,7 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :role, as: :select
       f.input :password
       f.input :password_confirmation
     end
