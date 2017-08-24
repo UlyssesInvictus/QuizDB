b872568cf1430390cdf346f3c816510c417bdf8f
Raynor Kuang <raynorkuang@gmail.com>
2017-08-24 00:08:55 -0400

    Do a little more implem of import form

--- app/admin/import.rb
+++ app/admin/import.rb
@@ -1,9 +1,17 @@
 ActiveAdmin.register_page "Import" do
+
   content do
     render partial: "admin/tossups/import.html.erb"
   end
 
   page_action :force, method: :post do
-    redirect_to admin_tossups_path, notice: 'test'
+    if params[:parsed_questions].present?
+      # do parsing
+      redirect_to admin_tossups_path, notice: 'questions added!'
+    else
+      # do parsing
+      @parsed_questions = "stuff"
+      render partial: "admin/tossups/import.html.erb"
+    end
   end
 end
