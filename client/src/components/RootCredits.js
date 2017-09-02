e2ba7c37052ceb6290ea72668cbae12fc1138953
Raynor Kuang <raynorkuang@gmail.com>
2017-09-02 01:15:45 -0400

    Remove deprecated attributes on paypal code

--- client/src/components/RootCredits.js
+++ client/src/components/RootCredits.js
@@ -23,8 +23,8 @@ class Root extends React.Component {
         <input type="hidden" name="cmd" value="_s-xclick"/>
         <input type="hidden" name="hosted_button_id" value="TEEB54JVFRKRL"/>
         <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-               border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+               name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+        <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
       </form>
 
     </Container></div>
