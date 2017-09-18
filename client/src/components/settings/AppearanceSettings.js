1545062d655429af0a950dbeafbba30b8ed36fed
Raynor Kuang <raynorkuang@gmail.com>
2017-09-17 21:00:51 -0400

    Working settings for search highlighting

--- client/src/components/settings/AppearanceSettings.js
+++ client/src/components/settings/AppearanceSettings.js
@@ -4,19 +4,32 @@ import { setStorage } from '../../actions/StorageActions';
 
 import {
   Segment,
-  Label
+  Label,
+  Grid,
+  Checkbox
 } from 'semantic-ui-react';
 
 class AppearanceSettings extends React.Component {
 
   render() {
+    const dispatch = this.props.dispatch;
+    const settings = this.props.storage;
 
     return <div className="quizdb-settings-storage">
       <Segment size="large">
         <Label attached='top'><h3>Appearance</h3></Label>
-        <p>
-          appearance settings go here
-        </p>
+        <Grid columns={3} stackable>
+          <Grid.Column>
+            <Checkbox
+              label="Highlight matching results in search"
+              checked={!!settings.highlightSearch}
+              onChange={(_, value) => {
+                console.log(value);
+                dispatch(setStorage("highlightSearch", value.checked))
+              }}
+            />
+          </Grid.Column>
+        </Grid>
       </Segment>
     </div>
   }
