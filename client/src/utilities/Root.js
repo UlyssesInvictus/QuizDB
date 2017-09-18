dd9b09b2d9046bc4d33437e92cb1d90e5cc41995
Raynor Kuang <raynorkuang@gmail.com>
2017-09-17 22:40:29 -0400

    Add usage tips

--- client/src/utilities/Root.js
+++ client/src/utilities/Root.js
@@ -4,9 +4,50 @@ import moment from "moment";
 import { setStorage } from '../actions/StorageActions';
 
 const USAGE_TIPS = [
-  "test1",
-  "test2",
-  "test3"
+  `
+    While viewing search results, you can search directly for an answerline on Google or Wikipedia
+    by clicking the relevant icon at the bottom of the question.
+  `,
+  `
+    You can help improve QuizDB! Consider applying to be an admin in the Admin portal, or
+    reporting errors in questions as you notice them!
+  `,
+  `
+    You can view Stats in QuizDB! You'll be able to see the usage history of questions by
+    category and difficulty, as well as find the most relevant keywords to those questions.
+  `,
+  `
+    You can use QuizDB offline! Visit the Settings page to download offline questions that can
+    be used even when you don't have an internet connection.
+  `,
+  `
+    You can manage your personal settings for QuizDB from the Settings page! These settings
+    will persist for your current device, but not across different ones.
+  `,
+  `
+    Certain question searches have been known to trigger new interactive content. Will you be
+    able to find the secret keywords?...
+  `,
+  `
+    QuizDB can be used as a mobile app on Android devices! Use the "Add to Home Page" feature
+    in Chrome mobile to access QuizDB natively through your phone.
+  `,
+  `
+    QuizDB has a huge list of external Quizbowl resources! Visit the Resources page to
+    read through them all.
+  `,
+  `
+    You can help build QuizDB by sending in feedback or even contributing code! See the About
+    page to learn more.
+  `,
+  `
+    QuizDB automatically highlights search results matching your search input. You can disable
+    this in settings.
+  `,
+  `
+    These tips can be disabled by clicking the button below, or toggled at any time from the Settings
+    page.
+  `
 ];
 
 export function showUsageTip(dispatch, storage) {
@@ -25,6 +66,10 @@ export function showUsageTip(dispatch, storage) {
       title: "QuizDB Usage Tips",
       message: USAGE_TIPS[Math.floor(Math.random() * USAGE_TIPS.length)],
       autoDismiss: 10,
+      action: {
+        label: "Stop showing these tips.",
+        callback: () => dispatch(setStorage("usageTips", false))
+      }
     }));
   }
 }
