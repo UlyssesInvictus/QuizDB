0ac01721b7576ae6bce715210722978cf7d68655
Raynor Kuang <raynorkuang@gmail.com>
2017-10-01 14:20:08 -0400

    ñ handling

--- client/src/utilities/Question.js
+++ client/src/utilities/Question.js
@@ -19,6 +19,7 @@ export function cleanSpecial(str) {
   newStr = newStr.replace(/畔/g, "f");
   newStr = newStr.replace(/㱀/g, "f");
   newStr = newStr.replace(/Ã¼/g, "ü");
+  newStr = newStr.replace(/Ã±/g, "ñ");
   return newStr;
 }
 
