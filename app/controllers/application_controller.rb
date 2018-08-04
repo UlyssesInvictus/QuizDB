a89a71998da1adfcfc3073826d3d4da64755496a
Raynor Kuang <raynorkuang@gmail.com>
2018-08-03 22:08:04 -0400

    Fix client error submission
    New 5.2 defaults to having forgery protection; I inadvertently added the
    defaults in application.rb (since I had no idea how those defaults work)
    , and this recognizes that and explicitly skips/disables forgery
    protection in the ApplicationController.
    
    https://stackoverflow.com/questions/50102584/upgrading-rails-what-am-i-to-do-with-new-framework-defaults-file
    
    There could be other stuff broken, but we'll fix that when we find it...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
   def fallback_index_html
     render :file => 'public/index.html'
