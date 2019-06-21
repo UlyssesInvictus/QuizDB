6b0dc5d26b2210df3886fd3af4e5e8489b670977
Noah <noahbres@gmail.com>
2019-06-21 00:24:55 -0500

    Removed the comments for buildQuery

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -128,22 +128,17 @@ class SearchForm extends React.Component {
   }
 
   buildQuery(data, filterEmpty = true) {
-    // If the data is already a string, return it as-is
 	  if (typeof (data) === 'string') return data;
 
-    // Create a query array to hold the key/value pairs
     const query = [];
 
-    // Loop through the data object
     for (let key in data) {
       if (data.hasOwnProperty(key) && (!filterEmpty || (Array.isArray(data[key]) && data[key].length > 0))) {
 
-        // Encode each key and value, concatenate them into a string, and push them to the array
         query.push(encodeURIComponent(key) + '=' + encodeURIComponent(data[key]));
       }
     }
 
-    // Join each item in the array with a `&` and return the resulting string
     return query.join('&');
   }
 
